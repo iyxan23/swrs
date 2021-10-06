7ae564cefd4d2fa80135702a2880183c29d6506a
Iyxan23 <nurihsanalghifari@gmail.com>
2021-10-06 09:56:13 +0700

    feat: implement encryption

---
+++ examples/encrypt.rs
@@ -0,0 +1,36 @@
+extern crate swrs;
+
+use std::process::exit;
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 && args.len() != 2 {
+        eprintln!(
+            r#"Usage: {} (FILE TO ENCRYPT) [OUTPUT]
+
+OUTPUT will be "(FILE TO ENCRYPT)_enc" if not specified"#, args.get(0).unwrap()
+        );
+        exit(1);
+    }
+
+    let input = args.get(1).unwrap();
+    let default_output = format!("{}_enc", input);
+    let output = args.get(2).unwrap_or(&default_output);
+
+    let input_path = Path::new(input);
+    let output_path = Path::new(output);
+
+    println!("Saving as {}", output);
+
+    let result = swrs::encrypt_sw_file_to_file(input_path, Option::Some(output_path));
+
+    if let Err(e) = result {
+        eprintln!("Failed to encrypt: {:?}", e);
+        exit(1);
+    } else {
+        println!("Successfully encrypted {}", input);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -32,3 +32,38 @@ pub fn decrypt_sw_encrypted(data: &Vec<u8>) -> SWRSResult<Vec<u8>> {
         Ok(Vec::from(plaintext_res.unwrap()))
     }
 }
+
+pub fn encrypt_sw_file_to_file(file: &Path, out: Option<&Path>) -> SWRSResult<()> {
+    let output_file = out.unwrap_or_else(|| file);
+
+    let input_plaintext = std::fs::read(file);
+    if input_plaintext.is_err() {
+        return Err(SWRSError::IOError(input_plaintext.unwrap_err()));
+    }
+
+    let output_ciphertext = encrypt_sw(&input_plaintext.unwrap())?;
+
+    let write_result = std::fs::write(output_file, output_ciphertext);
+    if write_result.is_err() {
+        Err(SWRSError::IOError(write_result.unwrap_err()))
+    } else {
+        Ok(())
+    }
+}
+
+pub fn encrypt_sw_file(file: &Path) -> SWRSResult<Vec<u8>> {
+    let input_plaintext = std::fs::read(file);
+    if input_plaintext.is_err() {
+        return Err(SWRSError::IOError(input_plaintext.unwrap_err()));
+    }
+
+    let output_ciphertext = encrypt_sw(&input_plaintext.unwrap())?;
+    Ok(output_ciphertext)
+}
+
+pub fn encrypt_sw(data: &Vec<u8>) -> SWRSResult<Vec<u8>> {
+    let cipher = Aes128Cbc::new_from_slices(KEY.as_ref(), KEY.as_ref())
+        .expect("Failed to create the AES key");
+
+    Ok(cipher.encrypt_vec(&*data))
+}
