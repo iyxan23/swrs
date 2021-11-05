d0e0de9ce3abfb2db4d8a809e717de4b1bfc8aff
Iyxan23 <nurihsanalghifari@gmail.com>
2021-11-05 14:32:40 +0700

    feat(parser): create (Raw)SketchwareProject struct

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,3 +1,4 @@
+use crate::error::SWRSError;
 use super::error::SWRSResult;
 
 pub mod project;
@@ -17,5 +18,87 @@ pub trait Parsable {
     /// by default, if not implemented, this will panic ([`unimplemented!()`])
     fn reconstruct(&self) -> SWRSResult<String> {
         unimplemented!()
+    }
+}
+
+/// Represents a raw (un-parsed) sketchware project
+pub struct RawSketchwareProject {
+    pub project: String,
+    pub file: String,
+    pub library: String,
+    pub resource: String,
+    pub view: String,
+    pub logic: String,
+}
+
+impl RawSketchwareProject {
+    /// Creates a RawSketchwareProject with the specified fields
+    pub fn new(project: String, file: String, library: String, resource: String, view: String, logic: String) -> Self {
+        RawSketchwareProject { project, file, library, resource, view, logic }
+    }
+
+    pub fn from_encrypted(
+        project: Vec<u8>,
+        file: Vec<u8>,
+        library: Vec<u8>,
+        resource: Vec<u8>,
+        view: Vec<u8>,
+        logic: Vec<u8>
+    ) -> SWRSResult<Self> {
+        macro_rules! decrypt {
+            ($name_ident:ident, $name:expr) => {
+                String::from_utf8(super::decrypt_sw_encrypted(&$name_ident)?)
+                    .map_err(|e| SWRSError::ParseError(format!(
+                        "Failed to decode {} due to an encoding error: {}", $name, e
+                    )))?
+            }
+        }
+
+        Ok(RawSketchwareProject {
+            project: decrypt!(project, "project"),
+            file: decrypt!(file, "file"),
+            library: decrypt!(library, "library"),
+            resource: decrypt!(resource, "resource"),
+            view: decrypt!(view, "view"),
+            logic: decrypt!(logic, "logic"),
+        })
+    }
+}
+
+/// Represents a parsed sketchware project that contains
+/// [`project::Project`], [`file::File`], [`library::Library`], [`resource::Resource`],
+/// [`view::View`], and [`logic::Logic`]
+pub struct SketchwareProject {
+    pub project: project::Project,
+    pub file: file::File,
+    pub library: library::Library,
+    pub resource: resource::Resource,
+    pub view: view::View,
+    pub logic: logic::Logic,
+}
+
+impl SketchwareProject {
+    /// Parses a [`RawSketchwareProject`] into [`SketchwareProject`]
+    pub fn parse_from(raw_swproj: RawSketchwareProject) -> SWRSResult<Self> {
+        Ok(SketchwareProject {
+            project: project::Project::parse(raw_swproj.project.as_str())?,
+            file: file::File::parse(raw_swproj.file.as_str())?,
+            library: library::Library::parse(raw_swproj.library.as_str())?,
+            resource: resource::Resource::parse(raw_swproj.resource.as_str())?,
+            view: view::View::parse(raw_swproj.view.as_str())?,
+            logic: logic::Logic::parse(raw_swproj.logic.as_str())?
+        })
+    }
+
+    /// Parses a list of project data into [`SketchwareProject`]
+    pub fn parse(project: &str, file: &str, library: &str, resource: &str, view: &str, logic: &str) -> SWRSResult<Self> {
+        Ok(SketchwareProject {
+            project: project::Project::parse(project)?,
+            file: file::File::parse(file)?,
+            library: library::Library::parse(library)?,
+            resource: resource::Resource::parse(resource)?,
+            view: view::View::parse(view)?,
+            logic: logic::Logic::parse(logic)?
+        })
     }
 }
