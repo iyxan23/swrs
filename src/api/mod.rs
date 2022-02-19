c3edbfbd8ca7e56c759f44622e32ddfd437bc739
Iyxan23 <nurihsanalghifari@gmail.com>
2022-02-19 15:06:50 +0700

    wip(parser): implement the new error system
    huuuh, this is going to take a long time

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -542,7 +542,7 @@ impl TryFrom<SketchwareProject> for ParsedSketchwareProject {
                 fabs
             },
             logic: parser::logic::Logic { screens: logic_screens },
-            resources: Default::default()
+            resource_files: Default::default()
         })
     }
 }
