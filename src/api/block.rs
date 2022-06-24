aa7bd67904c7c16dab783e0c03e49f0d714413c5
Iyxan23 <nurihsanalghifari@gmail.com>
2022-06-24 23:07:42 +0700

    wip(api/block): implement some funcs of the new blocks api

--- src/api/block.rs
+++ src/api/block.rs
@@ -1,5 +1,5 @@
 use std::collections::btree_map::Range;
-use std::collections::BTreeMap;
+use std::collections::{Bound, BTreeMap};
 use std::num::ParseIntError;
 use std::ops::RangeBounds;
 use std::str::FromStr;
@@ -7,15 +7,27 @@ use thiserror::Error;
 use crate::color::Color;
 use crate::parser::logic::BlockContainer;
 
+/// The default start id of a [`Blocks`]
+const DEFAULT_BLOCK_ID_START: BlockId = BlockId(10);
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Blocks {
     blocks: BTreeMap<BlockId, BlockEntry>,
+    starting_id: BlockId,
 }
 
 impl Blocks {
     /// Creates a new [`Blocks`] instance
     pub fn new() -> Self {
-        Self { blocks: BTreeMap::new() }
+        Self {
+            blocks: BTreeMap::new(),
+            starting_id: DEFAULT_BLOCK_ID_START
+        }
+    }
+
+    /// Creates a new [`Blocks`] instance that will start with the specified starting id
+    pub fn new_w_start(starting_id: BlockId) -> Self {
+        Self { blocks: BTreeMap::new(), starting_id }
     }
 
     /// Compacts blocks after the specified id.
@@ -81,8 +93,10 @@ impl Blocks {
         todo!()
     }
 
-    /// Shifts blocks after the specified block into a specified number amount of time. Does not
-    /// compact the blocks that are shifted.
+    /// Shifts blocks after the specified block into a specified number amount of time (exclusive).
+    /// Does not compact the blocks that are shifted.
+    ///
+    /// Returns the first shifted block.
     ///
     /// ### Panic
     /// Panics when there is no space for a backwards/negative shift.
@@ -107,6 +121,8 @@ impl Blocks {
     /// a -> [10, 11, 12, 15, 17]
     ///               +5  +5  +5
     /// a -> [10, 11, 17, 20, 22]
+    ///
+    /// returns 17 ---^^
     /// ```
     /// ```txt
     /// a -> [10, 11, 12, 20, 26]
@@ -115,19 +131,121 @@ impl Blocks {
     /// a -> [10, 11, 12, 20, 26]
     ///                   -6  -6
     /// a -> [10, 11, 17, 14, 20]
+    ///
+    /// returns 14 -------^^
     /// ```
     pub fn shift_blocks(&mut self, after: BlockId, shift: i32) -> Option<BlockId> {
-        todo!()
+        // todo: check if we'll be surpassing the starting_id
+        if shift.is_negative() {
+            // check if there is a space to the back
+            let block_before = self.blocks.range(..=after).rev().next();
+            if let Some((block_before_id, _)) = block_before {
+                if *block_before_id >= BlockId(after.0 + shift) {
+                    panic!("cannot shift {} to the back because there is a block in the way (id {block_before_id})", shift * 1)
+                }
+            }
+        }
+
+        // take the blocks after the given block id
+
+        let blocks = self.blocks.range(after..)
+            .map(|(id, _)| id).collect::<Vec<BlockId>>().into_iter()
+            .map(|id| self.blocks.remove(&id).unwrap())
+            .map(|mut block| {
+                block.id.0 += shift;
+                block
+            })
+            .collect::<Vec<BlockEntry>>();
+
+        // re-add them with their new shifted ids
+
+        let mut first_id = None;
+        for block in blocks {
+            if let None = first_id { first_id = Some(block.id); }
+
+            self.blocks.insert(block.id, block);
+        }
+
+        first_id
     }
 
-    /// Inserts a block after the block with the specified id. Will shift the blocks after it when
-    /// there is no space.
+    /// Inserts a block after the block with the specified id.
+    ///
+    /// ### Behavior
+    /// If there is a block with the id of the given `after` parameter, the new ID will be after + 1
+    ///
+    /// Otherwise it will find if there is any block before the specified after parameter, and use
+    /// that block's id plus 1. If there is no block, it will use the Blocks' starting id.
+    ///
+    /// After all that, it will check if the determined ID is already occupied by another block,
+    /// if it's occupied it will shift the blocks by one to make for one space for the inserted
+    /// block
+    ///
+    /// ### Examples
+    /// ```text
+    /// Regular insertion
+    /// blocks -> [10, 11, 14, 17]
+    /// blocks.insert_block(block, after: 14)
+    ///                       v
+    /// blocks -> [10, 11, 14, 17]
+    /// blocks -> [10, 11, 14,   , 17]
+    /// blocks -> [10, 11, 14, 15, 17]
+    /// ```
+    /// ```text
+    /// Insertion with shifting
+    /// blocks = [10, 11, 12, 14]
+    /// blocks.insert_block(block, after: 11)
+    ///                    vv
+    /// blocks -> [10, 11, 12, 14]
+    /// blocks -> [10, 11,   , 13, 14] <- shifts
+    /// blocks -> [10, 11, 12, 13, 14]
+    /// ```
+    /// ```text
+    /// Insertion after a non-existent block
+    /// blocks -> [10, 11, 13, 18]
+    /// blocks.insert_block(block, after: 15)
+    ///                       v
+    /// blocks -> [10, 11, 13, 18]
+    /// blocks -> [10, 11, 13,   , 18]
+    /// blocks -> [10, 11, 13, 16, 18] <- 16 because 15 + 1
+    /// ```
+    /// ```text
+    /// Insertion on an empty blocks
+    /// blocks -> []
+    /// blocks.insert_block(block, after: 15)
+    ///
+    /// blocks -> [10] <- no matter what `after` is, its id will always be the starting id
+    /// ```
     pub fn insert_block(
         &mut self,
         block: Block,
         after: BlockId
-    ) -> Option<BlockId> {
-        todo!()
+    ) -> BlockId {
+        let id = if let Some(block_before) = self.blocks.get(&after) {
+            block_before.id.increment()
+        } else {
+            // this block doesn't exist
+            // if there is a block behind somewhere, we could do this operation and just set this
+            //   block's id to be after.increment()
+            // if not, we set the id to be the starting id
+            if let Some(_) = self.blocks.range(..after).rev().next() {
+                after.increment()
+            } else {
+                self.starting_id
+            }
+        };
+
+        // check if that id is already occupied
+        if let Some(_) = self.blocks.get(&id) {
+            // shift the blocks after it by one
+            self.shift_blocks(after, 1);
+        }
+
+        self.blocks.insert(id, BlockEntry {
+            id, block, sub_stack1: None, sub_stack2: None
+        });
+
+        id
     }
 
     /// Pushes a block on the back of the chain. The block id is retrieved through the addition of
@@ -136,17 +254,55 @@ impl Blocks {
         &mut self,
         block: Block
     ) -> BlockId {
-        todo!()
+        self.push_back_w_sub_stack(block, (None, None))
     }
 
     /// Pushes a block on the back of the chain with its substacks. The block id is retrieved
     /// through the addition of one on the last block.
+    ///
+    /// ### Panics
+    /// Panics when sub_stacks is given a tuple of (None, Some(_))
     pub fn push_back_w_sub_stack(
         &mut self,
         block: Block,
         sub_stacks: (Option<Blocks>, Option<Blocks>)
     ) -> BlockId {
-        todo!()
+        // todo: utilise BTreeMap::last_key_value() once the feature `map_first_last` (62924) got
+        //       stabilised
+
+        // to kind of prevent undefined behavior
+        if sub_stacks.0.is_none() && sub_stacks.1.is_some() {
+            panic!("the second substack cannot be some if the first substack is none");
+        }
+
+        let id = if let Some((last_block_id, _)) = self.blocks.iter().max() {
+            last_block_id.increment()
+        } else {
+            // oh this blocks is empty
+            self.starting_id
+        };
+
+        let entry = BlockEntry { id, block, sub_stack1: None, sub_stack2: None };
+        self.blocks.insert(id, entry);
+
+        // temporarily get a mutable reference to change the substack ids once we've inserted them
+        // into the chain
+        let entry = self.blocks.get_mut(&id).unwrap();
+
+        // add our substacks
+        let ss1_id = sub_stacks.0.map(|sub_stack1| {
+            self.insert_blocks(sub_stack1, id).unwrap()
+        });
+
+        let ss2_id = sub_stacks.1.map(|sub_stack2| {
+            self.insert_blocks(sub_stack2, ss1_id.unwrap()).unwrap()
+        });
+
+        // then change the ss1/ss2 ids on the block entry we've just inserted
+        entry.sub_stack1 = ss1_id;
+        entry.sub_stack2 = ss2_id;
+
+        id
     }
 
     /// Retrieves a block with the given id
@@ -159,16 +315,38 @@ impl Blocks {
         self.blocks.get_mut(&id)
     }
 
-    /// Removes the block with the given id and its substacks. Will not compact the blocks after
-    /// the removed block.
+    /// Removes the block with the given id and its substacks.
     pub fn remove(&mut self, id: BlockId) -> Option<(Block, (Option<Blocks>, Option<Blocks>))> {
-        todo!()
+        let block = self.blocks.remove(&id)?;
+
+        Some((block.block, (
+            block.sub_stack1.map(|sub_stack1| self.remove_ranged(id..sub_stack1)).flatten(),
+            block.sub_stack1.map(|sub_stack1| self.remove_ranged(id..sub_stack1)).flatten(),
+        )))
     }
 
     /// Removes a range of blocks
-    pub fn remove_ranged<R>(&mut self, range: R) -> Option<Blocks>
+    pub fn remove_ranged<R>(&mut self, range: R) -> Blocks
     where R: RangeBounds<BlockId> {
-        todo!()
+        // used to skip substacks
+        let mut skip_until = None;
+        let mut removed_blocks = Blocks::new();
+
+        for (id, entry) in self.blocks.range(range) {
+            if let Some(skip_until_) = skip_until {
+                if id != skip_until_ { continue; }
+                skip_until = None;
+                continue;
+            }
+
+            if let Some(sub_stack1) = entry.sub_stack1 { skip_until = Some(sub_stack1); }
+            if let Some(sub_stack2) = entry.sub_stack2 { skip_until = Some(sub_stack2); }
+
+            let (removed_block, substacks) = self.remove(*id).unwrap();
+            removed_blocks.push_back_w_sub_stack(removed_block, substacks);
+        }
+
+        removed_blocks
     }
 
     /// Removes an amount of blocks after a block with the given id
@@ -198,6 +376,11 @@ impl Blocks {
     /// Will compact the blocks after the inserted substack with [`Blocks::compact_blocks`]
     pub fn replace_sub_stack2(&mut self, id: BlockId, blocks: Blocks) -> Option<BlockId> {
         todo!()
+    }
+
+    /// Returns how much blocks are there that are stored in this [`Blocks`]
+    pub fn length(&self) -> usize {
+        self.blocks.len()
     }
 }
 
@@ -255,6 +438,18 @@ impl TryInto<BlockContainer> for Blocks {
 
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq)]
 pub struct BlockId(pub u32);
+
+impl BlockId {
+    /// Increments [`BlockId`] by one
+    pub fn increment(&self) -> Self {
+        BlockId(self.0 + 1)
+    }
+
+    /// Increments [`BlockId`] by the given amount of number
+    pub fn increment_by(&self, num: u32) -> Self {
+        BlockId(self.0 + num)
+    }
+}
 
 impl FromStr for BlockId {
     type Err = ParseIntError;
